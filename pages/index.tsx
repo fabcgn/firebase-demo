b073b2a8dd9152c7e071993db4cafa576c2d90f8
Fabian Seibt <5725591-fabcgn@users.noreply.gitlab.com>
2022-04-04 15:44:07 +0200

    Moved components folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from "next"
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
-import Admin from "./components/Admin"
-import Public from "./components/Public"
+import Admin from "../components/Admin"
+import Public from "../components/Public"
 
 const Home: NextPage = () => {
   return (
